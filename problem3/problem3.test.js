d766d16085254411be08db5e3cc614ee31161a12
Devanada <ydevanada@gmail.com>
2021-12-21 14:59:39 +0700

    Fixed test case on problem 3

--- problem3/problem3.test.js
+++ problem3/problem3.test.js
@@ -38,30 +38,3 @@ test("Test 3", () => {
     )
   ).toEqual([]);
 });
-test("Test 4", () => {
-  expect(
-    playingDomino(
-      [
-        [2, 4],
-        [2, 6],
-        [3, 6],
-        [2, 1],
-      ],
-      [2, 1]
-    )
-  ).toEqual([2, 4]);
-});
-test("Test 5", () => {
-  expect(
-    playingDomino(
-      [
-        [4, 4],
-        [5, 4],
-        [5, 5],
-        [3, 4],
-        [2, 4],
-      ],
-      [5, 5]
-    )
-  ).toEqual([5, 4]);
-});
